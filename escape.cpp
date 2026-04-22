9b803dfa3075756af090efd0291df2e551454376
s1140518-max <s1140518@dmhs.kh.edu.tw>
2026-04-22 15:15:59 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+    cout << "1happy" << endl;
+    cout << "2happy" << endl;
+    cout << "3happy" << endl;
+    cout << "4happy" << endl;
+    cout << "5happy\\" << endl;
+    cout << "6happy\" << endl;
+    return 0;
+}
