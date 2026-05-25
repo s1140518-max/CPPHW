6a9bfbc926e1dbb5a8ffacf639c90bc1a55fda62
s1140518-max <s1140518@dmhs.kh.edu.tw>
2026-05-25 09:47:35 +0800

    Create 99table.cpp

---
+++ 99table.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    for (int i = 1; i <= 9; ;++) {
+	    for (int j = 1; J <= 9; J++) {
+		    cout << i << " * " << j << " = " << i * j << "\t";
+        }
+        cout << end;
+    }
+    return 0;
+}
